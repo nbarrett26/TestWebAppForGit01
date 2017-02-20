f5bcc374d342b4a350426e0d7f43c49b4fc141e8
Nick <Nick@BARRETTLAPTOP2>
2017-02-20 17:35:22 -0600

    Add project files.

---
+++ TestWebAppForGit01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestWebAppForGit01.MvcApplication" Language="C#" %>
---
+++ TestWebAppForGit01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestWebAppForGit01.Startup))]
+namespace TestWebAppForGit01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
